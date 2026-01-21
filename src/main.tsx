1bc710ff14879ff87e6b6d0a76f4ff355562e661
shrdul-idk45 <shrdulchavanke@gmail.com>
2026-01-22 00:53:38 +0530

    FinanceFlow: full-stack expense tracker

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app/App";
+import { AppProvider } from "./app/context/AppContext";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>
+);
